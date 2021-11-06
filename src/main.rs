afdf0b57ff2fe0da831967aafeaa484d87ad16b4
Xiaohanxu1996 <hanxu1996a@gmail.com>
2021-11-06 20:02:55 +0800

    feat: swith on/off raw mode

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+use crossterm::{terminal, event};
+use crossterm::event::{Event, KeyCode, KeyEvent};
+use std::time::Duration;
+
+struct CleanUp;
+
+impl Drop for CleanUp {
+    fn drop(&mut self) {
+        terminal::disable_raw_mode().expect("Could not disable raw mode");
+    }
 }
+
+fn main() -> crossterm::Result<()> {
+    let _clean_up = CleanUp;
+
+    terminal::enable_raw_mode()?;
+    
+    loop {
+        if event::poll(Duration::from_millis(500))? {            
+           if let Event::Key(event) = event::read()? {
+               match event {
+                   KeyEvent {
+                       code: KeyCode::Char('q'),
+                       modifiers: event::KeyModifiers::NONE,
+                   } => break,
+                   _ => {
+                   }
+               }
+               println!("{:?}\r", event);
+           }
+        } else {
+            println!("No input yet\r");
+        }
+    }
+    Ok(())
+} 
