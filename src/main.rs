6be85257034de4fdcb8e583bf70241b9e09667f6
Xiaohanxu1996 <hanxu1996a@gmail.com>
2021-11-07 02:43:13 +0800

    feat: display welcome message

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,164 @@
-use crossterm::{terminal, event};
-use crossterm::event::{Event, KeyCode, KeyEvent};
+use crossterm::{terminal, event, execute, cursor, queue};
+use std::io::{self, Write};
+use crossterm::event::*;
+use crossterm::terminal::ClearType;
 use std::time::Duration;
+use std::io::stdout;
+
+struct Editor {
+    reader: Reader,
+    output: Output,
+}
+
+impl Editor {
+    fn new() -> Self {
+        Self {
+            reader: Reader,
+            output: Output::new(),
+        }
+    }
+
+    fn process_keypress(&self) -> crossterm::Result<bool> {
+        match self.reader.read_key() ? {
+            KeyEvent {
+                code: KeyCode::Char('q'),
+                modifiers: event::KeyModifiers::CONTROL,
+            } => return Ok(false),
+            _ => {}
+        }
+        Ok(true)
+    }
+    
+    fn run(&mut self) -> crossterm::Result<bool> {
+        self.output.refresh_screen()?;
+        self.process_keypress()
+    }
+}
+
+struct Reader;
+
+impl Reader {
+    fn read_key(&self) -> crossterm::Result<KeyEvent> {
+        loop {
+            if event::poll(Duration::from_millis(500))? {
+                if let Event::Key(event) = event::read() ? {
+                    return Ok(event);
+                }
+            }
+        }
+    }
+}
+
+struct Output {
+    win_size: (usize, usize),
+    editor_contents: EditorContents,
+}
+
+impl Output {
+    fn new() -> Self {
+        let win_size = terminal::size()
+            .map(|(x, y)| (x as usize, y as usize))
+            .unwrap();
+        Self {
+            win_size,
+            editor_contents: EditorContents::new(),
+        }
+    }
+    
+    fn clear_screen() -> crossterm::Result<()> {
+        execute!(stdout(), terminal::Clear(ClearType::All))?;
+        execute!(stdout(), cursor::MoveTo(0, 0))
+    }
+
+    fn draw_rows(&mut self) {
+        let screen_row = self.win_size.1;
+        let screen_column = self.win_size.0;
+        for i in 0..screen_row {
+            if i == screen_row / 3 {
+                let mut welcome = format!("Rust Text Editor");
+
+                if welcome.len()> screen_column {
+                    welcome.truncate(screen_column)
+                }
+                
+                let mut padding = (screen_column - welcome.len()) / 2;
+
+                if padding != 0 {
+                    self.editor_contents.push('~');
+                    padding -= 1
+                }
+                (0..padding).for_each(|_| self.editor_contents.push(' '));
+
+                self.editor_contents.push_str(&welcome);
+            } else {
+                self.editor_contents.push('~');
+            }
+
+            queue!(
+                self.editor_contents,
+                terminal::Clear(ClearType::UntilNewLine)
+            ).unwrap();
+
+            if i < screen_row - 1 {
+                self.editor_contents.push_str("\r\n");
+            }
+        }
+    }
+
+    fn refresh_screen(&mut self) -> crossterm::Result<()> {
+        queue!(self.editor_contents, cursor::Hide, cursor::MoveTo(0, 0))? ;
+        self.draw_rows();
+        queue!(self.editor_contents, cursor::MoveTo(0, 0), cursor::Show)?;
+        self.editor_contents.flush()
+    }
+}
+
+struct EditorContents {
+    content: String,
+}
+
+impl EditorContents {
+
+    fn new() -> Self {
+        Self {
+            content: String::new(),
+        }
+    }
+
+    fn push(&mut self, ch: char) {
+        self.content.push(ch)
+    }
+
+    fn push_str(&mut self, string: &str) {
+        self.content.push_str(string)
+    }
+}
+
+impl io::Write for EditorContents {
+    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+        match std::str::from_utf8(buf) {
+            Ok(s) => {
+                self.content.push_str(s);
+                Ok(s.len())
+            }
+            Err(_) => Err(io::ErrorKind::WriteZero.into())
+        }
+    }
+
+    fn flush(&mut self) -> io::Result<()> {
+        let out = write!(stdout(), "{}", self.content);
+        stdout().flush()?;
+        self.content.clear();
+        out
+    }
+}
 
 struct CleanUp;
 
 impl Drop for CleanUp {
     fn drop(&mut self) {
         terminal::disable_raw_mode().expect("Could not disable raw mode");
+        Output::clear_screen().expect("Error");
     }
 }
 
@@ -14,23 +166,10 @@ fn main() -> crossterm::Result<()> {
     let _clean_up = CleanUp;
 
     terminal::enable_raw_mode()?;
-    
-    loop {
-        if event::poll(Duration::from_millis(500))? {            
-           if let Event::Key(event) = event::read()? {
-               match event {
-                   KeyEvent {
-                       code: KeyCode::Char('q'),
-                       modifiers: event::KeyModifiers::NONE,
-                   } => break,
-                   _ => {
-                   }
-               }
-               println!("{:?}\r", event);
-           }
-        } else {
-            println!("No input yet\r");
-        }
-    }
+
+    let mut editor = Editor::new();
+
+    while editor.run() ? {}
+
     Ok(())
 } 
